2cafc173a2d7027c2cfe330d3b3db0fb34b062e3
MElvis87 <145340817+MElvis87@users.noreply.github.com>
2025-09-29 22:15:19 +0300

    Update
    Scroll Top Button

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -198,37 +198,29 @@ window.addEventListener('load', function() {
 
 
 //Scroll Top Button
-/**
-   * Scroll top button
-   */
-  let scrollTop = document.querySelector('.scroll-top');
+  //Get the button
+let mybutton = document.getElementById("btn-back-to-top");
 
-  function toggleScrollTop() {
-    if (scrollTop) {
-      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
-    }
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
-  scrollTop.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  });
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
 
-  window.addEventListener('load', toggleScrollTop);
-  document.addEventListener('scroll', toggleScrollTop);
-
-  /**
-   * Animation on scroll function and init
-   */
-  function aosInit() {
-    AOS.init({
-      duration: 600,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    });
-  }
-  window.addEventListener('load', aosInit);
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
   //Scroll Top Button End
